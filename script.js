7fb36d2c1df88c9c2e7cb65ad958eca62f213931
Mlakshay01 <142075858+Mlakshay01@users.noreply.github.com>
2024-06-18 17:14:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Handle service card click for mobile devices
-    const serviceCards = document.querySelectorAll('.service-card');
-
-    serviceCards.forEach(card => {
-        const readMoreButton = card.querySelector('.card-button');
-        readMoreButton.addEventListener('click', function(event) {
-            event.preventDefault();
-            card.classList.toggle('expanded');
-            const expandedDesc = card.querySelector('.expanded-description');
-            if (expandedDesc) {
-                if (card.classList.contains('expanded')) {
-                    expandedDesc.style.maxHeight = expandedDesc.scrollHeight + "px";
-                    expandedDesc.style.opacity = 1;
-                } else {
-                    expandedDesc.style.maxHeight = 0;
-                    expandedDesc.style.opacity = 0;
-                }
-            }
-        });
-    });
-
-    // Observer for pricing cards
     const pricingCards = document.querySelectorAll('.pricing-card');
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('slide-in');
-                observer.unobserve(entry.target);
-            }
-        });
-    }, { threshold: 0.01 });
 
-    pricingCards.forEach(card => {
-        observer.observe(card);
-    });
+    function handleResize() {
+        if (window.innerWidth <= 700) {
+            pricingCards.forEach(card => {
+                card.classList.add('slide-in');
+                card.style.transform = 'none';
+            });
+        } else {
+            const observer = new IntersectionObserver((entries, observer) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add('slide-in');
+                        observer.unobserve(entry.target);
+                    }
+                });
+            }, { threshold: 0.01 });
+        
+            pricingCards.forEach(card => {
+                card.classList.remove('slide-in');
+                observer.observe(card);
+            });
+        }
+    }
+
+    handleResize();
+    
+   
+    window.addEventListener('resize', handleResize);
 });
